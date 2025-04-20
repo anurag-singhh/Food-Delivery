f4c532616ee4c54305cf66c8766b89863459024e
Anurag Singh <anusingh1104@gmail.com>
2025-04-20 12:25:36 +0530

    connection update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://anusingh1104:tomato11@food-del.abv4x.mongodb.net/food-del"
+      "mongodb+srv://anusingh1104:tomato11@food-del.r92flpb.mongodb.net/food-del"
     )
     .then(() => console.log("DB Connected"));
 };
